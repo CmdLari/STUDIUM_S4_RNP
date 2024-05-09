729dbbb5ebc7b8b382c11d0855b2e1a679367661
gottong <fabian.gottong@haw-hamburg.de>
2024-05-09 12:50:10 +0200

    Added Syslog

---
+++ src/SyslogException.java
@@ -0,0 +1,15 @@
+
+import java.lang.Exception;
+
+public class SyslogException extends Exception
+{
+    SyslogException()
+    {
+        super();
+    }
+
+    SyslogException( String msg )
+    {
+        super( msg );
+    }
+}
